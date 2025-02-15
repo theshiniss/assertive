5c288d70ac601d5aa9c5e433e8b939d7fc5f2469
TheShinISS <marco.massari@iss.it>
2025-02-15 23:46:00 +0100

    Upload Files

---
+++ dipendenze_assertive.R
@@ -0,0 +1,22 @@
+# download e installazione dipendenze pacchetto "assertive"
+# NB: l'ordine deve essere quello qui sotto, per ultima la libreria assertive ... le precedenti sono le sue dipendenze
+install.packages("https://cran.r-project.org/src/contrib/assertive.base_0.0-9.tar.gz",repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.properties/assertive.properties_0.0-5.tar.gz",repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.types/assertive.types_0.0-3.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.code/assertive.code_0.0-4.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.strings/assertive.strings_0.0-3.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.data/assertive.data_0.0-3.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.data.uk/assertive.data.uk_0.0-2.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.data.us/assertive.data.us_0.0-2.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive.datetimes/assertive.datetimes_0.0-3.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/assertive.numbers_0.0-2.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/assertive.files_0.0-2.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/assertive.sets_0.0-3.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/assertive.matrices_0.0-2.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/assertive.models_0.0-2.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/assertive.reflection_0.0-5.tar.gz", repos = NULL, type = "source")
+install.packages("https://cran.r-project.org/src/contrib/Archive/assertive/assertive_0.3-6.tar.gz", repos = NULL, type = "source")
+
+
+
+
---
+++ dipendenze_assertive_da locale.R
@@ -0,0 +1,41 @@
+# download e installazione dipendenze pacchetto "assertive"
+# NB: l'ordine deve essere quello qui sotto, per ultima la libreria assertive ... le precedenti sono le sue dipendenze
+library(here)
+library(devtools)
+
+install.packages(here::here("assertive.base_0.0-9.tar.gz"),repos = NULL, type = "source")
+install.packages(here::here("assertive.properties_0.0-5.tar.gz"),repos = NULL, type = "source")
+install.packages(here::here("assertive.types_0.0-3.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.code_0.0-4.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.strings_0.0-3.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.data_0.0-3.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.data.uk_0.0-2.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.data.us_0.0-2.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.datetimes_0.0-3.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.numbers_0.0-2.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.files_0.0-2.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.sets_0.0-3.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.matrices_0.0-2.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.models_0.0-2.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive.reflection_0.0-5.tar.gz"), repos = NULL, type = "source")
+install.packages(here::here("assertive_0.3-6.tar.gz"), repos = NULL, type = "source")
+
+
+library("assertive.base")
+library("assertive.properties")
+library("assertive.types")
+library("assertive.code")
+library("assertive.strings")
+library("assertive.data")
+library("assertive.data.uk")
+library("assertive.data.us")
+library("assertive.datetimes")
+library("assertive.numbers")
+library("assertive.files")
+library("assertive.sets")
+library("assertive.matrices")
+library("assertive.models")
+library("assertive.reflection")
+library("assertive")
+
+
